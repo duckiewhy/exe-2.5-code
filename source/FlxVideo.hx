7bd428c9aaaf34214c39a723bc4609eab7c1cc22
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-08 14:41:43 +0200

    update FlxVideo.hx

--- source/FlxVideo.hx
+++ source/FlxVideo.hx
@@ -70,8 +70,10 @@ class FlxVideo extends FlxBasic {
 
 		FlxG.stage.addEventListener(Event.ENTER_FRAME, fixVolume);
 		vlcBitmap.repeat = 0;
+	 #if android
 		vlcBitmap.inWindow = false;
 		vlcBitmap.fullscreen = false;
+	#end
 		fixVolume(null);
 
 		FlxG.addChildBelowMouse(vlcBitmap);
