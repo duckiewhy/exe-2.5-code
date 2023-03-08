6a3e17bf3a21b29bb7203e883bb0f9e0d90eb199
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-08 15:10:14 +0200

    update EncoreState.hx

--- source/EncoreState.hx
+++ source/EncoreState.hx
@@ -225,17 +225,3 @@ class EncoreState extends MusicBeatState // REWRITE FREEPLAY!?!?!? HELL YEA!!!!!
 		else if (curSelected > songArray.length - 1) curSelected = songArray.length - 1;
 	}
 }
-
-class SongMetadata
-{
-	public var songName:String = "";
-	public var week:Int = 0;
-	public var songCharacter:String = "";
-
-	public function new(song:String, week:Int, songCharacter:String)
-	{
-		this.songName = song;
-		this.week = week;
-		this.songCharacter = songCharacter;
-	}
-}
