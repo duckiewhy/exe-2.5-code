89fa55b5d59457b53b012fe96ada3bc22584bbfd
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-11 10:45:00 +0200

    breh

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,9 +1,5 @@
 package;
 
-// hello welcome to code for sonic.sex 2.5!!!!!!!!!!!!!!!!!!!!!!!!! i am currently shitting
-// HOLY FUCKLES ITS KNUCKLES
-// HIS NAME IS CHAOTIX!!!
-
 import flixel.system.scaleModes.RatioScaleMode;
 import FunkinLua;
 import Section.SwagSection;
@@ -1000,9 +996,6 @@ class PlayState extends MusicBeatState
 				porker.scrollFactor.set(1.4, 1);
 				porker.antialiasing = true;
 
-			//	unfinished, ill do later
-			//	fuck you fleetway your code is gibberish
-
 			case 'needle':
 				/**
 								READ HOODRATS YOU MONGALOIDS
@@ -1115,11 +1108,6 @@ class PlayState extends MusicBeatState
 				add(trueFatal);*/
 
 			case 'fuckles':
-				//HOLY FUCKLES ITS KNUCKLES
-				//also read softlocked by jesterfrog
-				//https://mspfa.com/?s=37540&p=1
-				//anyways its chucklefuck time
-
 				GameOverSubstate.deathSoundName = 'chaotix-death';
 				GameOverSubstate.loopSoundName = 'chaotix-loop';
 				GameOverSubstate.endSoundName = 'chaotix-retry';
@@ -1254,23 +1242,6 @@ class PlayState extends MusicBeatState
 				add(stardustFurnace);
 
 			case 'starved':
-				// fhjdslafhlsa dead hedgehogs
-
-				/*———————————No hedgehogs?———————————
-				⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝
-				⠸⡸⠜⠕⠕⠁⢁⢇⢏⢽⢺⣪⡳⡝⣎⣏⢯⢞⡿⣟⣷⣳⢯⡷⣽⢽⢯⣳⣫⠇
-				⠀⠀⢀⢀⢄⢬⢪⡪⡎⣆⡈⠚⠜⠕⠇⠗⠝⢕⢯⢫⣞⣯⣿⣻⡽⣏⢗⣗⠏⠀
-				⠀⠪⡪⡪⣪⢪⢺⢸⢢⢓⢆⢤⢀⠀⠀⠀⠀⠈⢊⢞⡾⣿⡯⣏⢮⠷⠁⠀⠀
-				⠀⠀⠀⠈⠊⠆⡃⠕⢕⢇⢇⢇⢇⢇⢏⢎⢎⢆⢄⠀⢑⣽⣿⢝⠲⠉⠀⠀⠀⠀
-				⠀⠀⠀⠀⠀⡿⠂⠠⠀⡇⢇⠕⢈⣀⠀⠁⠡⠣⡣⡫⣂⣿⠯⢪⠰⠂⠀⠀⠀⠀
-				⠀⠀⠀⠀⡦⡙⡂⢀⢤⢣⠣⡈⣾⡃⠠⠄⠀⡄⢱⣌⣶⢏⢊⠂⠀⠀⠀⠀⠀⠀
-				⠀⠀⠀⠀⢝⡲⣜⡮⡏⢎⢌⢂⠙⠢⠐⢀⢘⢵⣽⣿⡿⠁⠁⠀⠀⠀⠀⠀⠀⠀
-				⠀⠀⠀⠀⠨⣺⡺⡕⡕⡱⡑⡆⡕⡅⡕⡜⡼⢽⡻⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-				⠀⠀⠀⠀⣼⣳⣫⣾⣵⣗⡵⡱⡡⢣⢑⢕⢜⢕⡝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-				⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-				⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-				⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-				—————————————————————————————*/
 
 				GameOverSubstate.deathSoundName = 'starved-death';
 				GameOverSubstate.loopSoundName = 'starved-loop';
@@ -1423,90 +1394,6 @@ class PlayState extends MusicBeatState
 				satFgFlower.setGraphicSize(Std.int(satFgFlower.width * 0.85));
 				satFgFlower.scrollFactor.set(1.1, 0.9);
 			case 'hog':
-							//
-				/**
-				dQvJjL*J@$$YUfZ#C0YO%mtc*#wucC#b0qnUzzwdv0*UJYB$$80ccjUvuULZCpX#YYzahpcxY*n0nQ/JrL8JC$$@8nO*0zd*UW8huL#MY
-				@BBB@B%@@@@BBBB8B@@@@B@BB%B@@%%%B@@#BBBB@BB@%B@@$$@@@@B%@@@@BBBBB%@B@%@%@BBBBB@%@BBBB@@@BB@@%%@@B@BB@%8%%
-				raXwdbJUh8@/p|0acp0batwd*c|Cmpq(tJd|ZfUpt)jX)kMB@B#B*0pmpw*WMCxm*hW0ZfohCvnqdqjk*Xtz0q&@bUa8bamhX%n|fk#tf
-				wakpp*MpZdB$@mbw*WpZZkBZUb8ow0wp%q0ZmC#w0OQmaahM8$$@a*owhQpk#MMM#habo8#Zob&MOq8WLhhOmp0B$$%bW8*o&apB#bdw%
-				@@@@@@$@@@@$@@@@@@@@@@@@@@@@@@@@$@@$@8@@@@@@@@@@$$$$@@@@@@@@$@@@$@@@@@@$@B@@8@@@@@@@@@@@@$@@@$@@@@@@@@@@@
-				JdqCucj/qC@$$|LfqkcJQZ8/(Jh0r)tXbUf|rxXrrwum*JUU#$$BdMdzOn0U#dpZ0qYYd#hrzr0o(0UZ(x(rOU0@$$ouoMwwbwXBdtjJh
-				W88WMW#W%W@$@M&W%B&MW8@&*WB%8##&B8&&MMW&WW&%BWWW@$$@8%&M8#MW%8&&%%WMWB%8M#W%M8MWavOWB88$$$B&%%88%88@B88WB
-				W&&W##*k&W@$$hWa&&oW&&BkoM##kha#&ohdoaMha8oWBMMM8$$@%@%MW*%MB88W*W#M%%8hMo&%p*pt'  w#*#@$$8o%@M&&&o$WdkW8
-				vOqzru(jqz@$$/XfwkznYQ%x-xi+u1/ca0uvtjvcnLxZoXcX&@$BZwQxJ/vvpw0LCdcrQMbXxfzk~i`  " !bLZ@$$Mvb*OQkwc8oXvvh
-				%@@@@@@@@@@$@$@@@@@@@@@@@MI  .~c*@@@@@B@$@@@@@@@@@$@@@@@@@@@@@@@@@@@@@@@@Batl..,-p@J.(@$$$@@@@$@@@@@@@@@@
-				0ao#ZmOCOQa@$$aLM%hhhom@0+ )f' ,/pWpOY0hhq0M8d0pbh$$$q#w#hLow#$ZoM8wQwoMqx+!<_uxL&0z^;QWo$$@#B&*W&z&@80mo
-				}QcY~/1vZQC@$$avbom0pZX#` lrMpUOnj&JMQu)#q(0oj(U(%$$$(Y(JO1|]YB/~0*wt/n|J(}f{)On1f{Qj ;wM$$okw0OcYrpBbZdb
-				%B@@@@@@@@@@@$$@@@@@@@m< }&@@@@@@@@@@@B@@@@@@@@@@@@$$@@$@@@@@@@@@@@@@@@B@@@@@@@@@@@@@|:/o@$$$@@@@@@@@@@@@
-				ochnxaU/fux#d%$#Mh*ZCh`  x0mqrp*hxJbd#x]hbOL&0uYrpC$$$hU#MQmbqu$J{rOpt//r0QttnJL|)awXvC,"!$$duaojuzpJ@UmM
-				UY*OUccXzx1_>I!Irxhhz( lCp0W*LJW8*bZp8b0awoZoZUXwqk$$$Bh%8*#Www@omaU&ppLLp8dO?c)8LfmMkpd":$$hUaoQpCvUBJza
-				@B@8d0Uxzv1<.   ;O%h+xpo@@BBB@@@@B@@@@@%@BB@@@@@@@@@$B@B@@BB@@B@@@B@@@@@BX~~i."Z%B$@@J+Zbd@@@BB@BB@@BBB@@
-				vrtQ/0hXUXo@%r<` '1m.}BLQpo|({i"])rOp)pxr0n(zYudh$$$kJ#&LJkwJBw(fuZvZux_?      }fOhx].;aWhX1nc!i/L(o1fpYQ
-				h#aao&Moko@@@ha~  +_iQ%d0x[i.    .uddpWkdpqb#M#8%$@@B*%8#MW#MB%*Mh8o&-_^      .td#nI/I:}<"^.      `(ddWhB
-				88BB%%%%%@@B&W*h< ..i%8%^         'O#&&BB8&%B%8$$$@B@BBBB&@B@%88&BB8}          ]&8~    ^,'I)jj/~"^^^C%B%8
-				UxodxuXX0qc)l,. .   iJfv.           !v1cYujZMzY$$$%obdmqC/##*0ZCtp*[           '(x`   `Y#jLjpoUz00q-:cOdC
-				BBB@BBov}I;^ .I.'   iBBB,            <%BBBBB@B@*@$@@@@BBBB@@@@@BBB[             WBki   ."?MB@@BBB@@BdWB@@
-				m#MmQzf|{YZQv-'     >&&%0'            <opk#wqbanJB@#%MZW#p%8@bqbhr'            !qo%)   >'  +Xad##WBwo*8dZ
-				)(/)twhJqk~^  ^.    iLvCq/             ~?jt)cqrJ`/8W*k|OmxMM%dJJ].            .v0Zm:   IQ]' .,<(uw8f_0WwU
-				@B8BB@Box''i/ac..  "l8B@@O'             YB@@@BB&|lU@B<J@@@@@@@@0`             :MBM~'! I /%8u~' `O8@@BB@@@
-				ukoXzC/!.+mopO^'` ^)^L#&mm]             `jbuzLmWBQ^0Y-h8bY8MBCv;              [MU] -C`<t.-wcqXi'`{8cqa#Yn
-				x(nJz(Iim@$W| ,;  ,j ]zLakv"             :rxCoY$$k.'!kawqzW8%w.              -LU( IUv{ a+ nU#8q(l'~x[Z8bZ
-				@@BO1fL8@$$M<'r';`(@"[@@@@@U             .n@@@@@$8[ '8@@@@@@8+              Ia8{ ]M@@p^nk`lk@@@@8Uiz@@@@@
-				nUL]1UQUJ@B1.o~ } 0Cf XoJLjrI             'JoQ|$@%( -hp#Jt&#r              .c0".`uc1pn>i%} .wmrZap*:xCQv|
-				UQLphpp#8#L]_C{'n:tLU(1Cwdwmcf;          .<)0WW%aL1I}xa*OoMC'            .-pCf'_LQppMULn|Yxr{CQ0Wd0mmpwbm
-				%p8B$@@@@@M:L%I_*;{@@8 `vB@@@@U             `B@W@$di `W@@@@f I           IoB%[.zB@@@@@@0,@@%^,@@@@@@@o@@@
-				n)OLpOYqLJf/Mp.ht.[jhB]' ;_Jdow[             }*C@@*  "UCdw-..'         ;/ptn).?X%OnYXY0p^%u&< ((Zc%0r0Ykj
-				%*aW%8WWMh)W%Z(8v.pa&B#b} .{#&8%}            +*p@M}   lwq< ^          _o8%&p'<WMB&*oW#8k;@o%m"?*aoBWo**B*
-				**B&o##MW0o@%ZU%z'#*@B%8Wn. X&*&al          '",.,'        ^`    .,,.^~bo*o*r'Jo&8MaMBM*p?BW@&i_kB8@8&%#&M
-				Cx*kUvOrkk@8c#d}1utn@qX0xm: izwhxf.        '^       .   '"Iil", ..]c|r-1Yu~<(YwUcn(Xqv(nk8hBU:JxWa@qUmvdp
-				o@B&#%%MW$$@#*#i1o%o8%8W8%k,}o#@&@L;,"   I,l;    ^''|,,'         {~Ii1u%&Bi~p*%%WB&8Ba/a%B#kplo&&&@B8WM@@
-				mpM*Cph0hw@$@OoZJ'OMM#%LdYZLv^:J0wZ0hz);.       i-<.fp8U 'i_]]1{l~~~uLmz*U}.:cxaZdpObmmua$omWBj/&Ww@bLm0q
-				xZkLUn|cmc@$@|0cU!L1Qp%r+.XwO- |*LcunfwYnzv(^  ~vx^ZmBhct '+0YUXLqn1dM8Y1' ,)]"c{//vaarZ$$&Z&WtYoqZBoXpr#
-				#W@%&%&8BM@$@8M#J-W@WW@%M.'}0a>:8B&&*kOx(+.`l1wwx[UX(!1xWY> ^{cddM##aMqU^`x&0i,*&%W8B*d@@$B#W%Jh%B#B@B#%%
-				zLobtLwrZYB$@vbcJ]cook%r0|, "c].}cf}Ufz<?X_xkXr}O0,     Xxb<  .";;>l?-;."<Mk!"^qzJQuOzu@@$bX#Wnu#oJ@OfYnC
-				Yq*wOJcLdL@$@cwL0/mnmb%Utq*  rr uZJXYzkQLZma*Qxa0       rxZQWx_+l!i<}~ "fCUii/t0vccL#oQ$$$8w&&mh#kq%MOdJW
-				%%@BBBBB@B$@@B%%Mx%@BB@BBBB. wB`tBBB&kOLOk#88BBk:       qBBB%BB8**#Mz`~dBBWi~&BBBB%B@%B$@$@%BB%%@@%B@@%BB
-				bdtvLfwz@$$jdxL-vdpa#/YXJ' ]0,`+()Q)Jtz#nYWUxf.     `<X{bp8hohjUZ(,"Q1Z]u.{j0dxuYxmCf@$@pCo%Yjh*L@0fzf0c0
-				MhdwZd#d@$$mhwaQbwk#%d0b&? -Zx   '<tkpdok#Wawpr^ '<(QOd0db&ahak#q:'m%k[}? UbwkOwmd8*p@$@BkW8aaW8aB8hhp8o*
-				B@BB@BBB@$$B@BB%h@@@@B@B@v -%MmdI.  ;mBBBBB@BB@@@@@$@BBB@@@@@BB@c.OBBW+l" BBB@BB@BBBB@$@@B@$BB@@B@BBBBBBB
-				QpUw*Q|p&$$fduLUXL0oMrCJh{ "CU]ixu[^  >1YxLZnm*$$BoBMZu1pb&W*0tf`ihmb[:' ?cxdpCQac|YcM$@kLo%vYh#C$UrdjQtu
-				#*UOZOdJB$$OCrCa0*YL8ZzQ#h,.YZl`ZLXU|" .")MOJXW$$8ZvzXJmLmXO{I'' }Q*hL.I htvCaYwCLoJvB$@&n0*QYaMz&WbxqM*z
-				8B#M@Mh8B$$M%MWa&B%B@MBM&o[ nd<.Y%hQpWvi  {hhW%@@%ohOLU|I.     ,_u0W8/,kroW#8&%#BM#MMB$$&W8@*oB@W$M#%oh*M
-				adzoaJfLoW$&dJh*OhbbQ8Ut00k[<IX/">/i"iu/r/`:;_f(/+!:.      `I<(U*j]iCw_;0pL{a0Ymmufx/dM$%&#BaXdW0W&oxxZLU
-				dbwZ#LwZn%$$&xOoQvMkc%apXq%hz]/L  X|  0Qhuf],    .'^I><_|JUz%x;wW^:'Q} ]Z0arkUzZdQd0cka$@a0kYcpLMJBM@0a0#
-				Mo8hBhqaa&$$#hW@wd%Ba$bk%wOdhhjX. /#  Ud#l.bcnn(JQZ0a#&OM8#B(} ha  iol"Wo%8ZM#m#BadMbhW$$@&@8m*W*&$%hahmZ
-				kmc*kYrUk#$$pzk8UwpdzBv(J0&|t(qp> l/" I|c  vp0hzB%$fwjQ)fZx)  .Q)  [t.1xCpJ_aLtZQnft1q*$$%*%orp*0oBM(rpQc
-				*Moh&baamB$$%ZhWkw&#w%M*woBMq*h* ]Z[ !bb` rw#q?ctBwqr_ pkZ;  nb, Idix&dhkMO#pwa*k*kw#M$@Mh#pw*b&bB8@hWkW
-				%&aMh*%b$$$#&&oWhM%%@Mo#md8##tk{.Yd:.(8n' ohar` J8*n^.1#B?. [&+'.bUWqZM*M@O*h8M&dhBhh@$@B@B8hw8%B%B8qoWb@
-				nOdpUJXQ*@@#XwQjCz0XhJu0tWcvzu?Q!`+X"!-ut,i-hx_:i[wLi:^nuYt:;ru;"~0QZMcdQQCmxjW0vJuUq0@BB0On0aLZZq8LXXwaw
-				WBB%W8&8%@@@&WWM&88W&%&&M@8&&%##~ )MnoQ,;0*I:1M#Y',!**1^'mM*,,<#j`:MWMI/&B8%WWB8&8M8BW@@@%&M8%WB&8@%&W8B%
-				a&po%#dakhW$$@oB#dhbMh@#hMwWdd#*-'}mh%f:.Yo+ i#bJ  ^B&] 'Zkp '*h,'jMMZ ~k%*hk*dhB#mkbp#h@$$bobW#**WM$dha*
-				wp/b&mz0X0w$$Bvzn/UQ0xLqcJj8YvYd_.xLnZt> nZi ^hnr  ^W8: "Ou- 1oz._Yaki^UL&CbLOxX80zQrCdv@$@LvtZbxhJU@0vuw
-				8BB@BB@@@BB$$$@@@@BB@@@$@@B@BB@@f /%@B%u XBc .bB%  ^B@I ?BW+.J@a_MB%[,h%BB@BBB@BB@BB@BB@@$$B@B@B@@B@$B@@B
-				JOzpkJLk#C0$$@qBkLwZ*w@**hOawZ%#U Iphkcw nQC' nJa  ^Ba  r*0^<o@mUQbb:^z#habzUdMQZkqhMk0#*$@UwLokwdpk@XwoW
-				)Y?zzv{wh(X@$B(j([zu(/n0tLrorfxoL  ]OM}n([+z^ LrrI ^Wp .JZ[`pZBMwjvZ :nwCBc#xz+YdQ1ZOfv}&$@Yf}Qd/Q/j@n+uk
-				8B%@@%&BB%B$$$B$BB%B@@$@@@8BB%@B%l )@@M%%#BBZ,k%%f ;@u nBB88@@@BBB&J cB@%@%B%B8%$@&BB%B%$$$BBBB%B@@@$BBB8
-				#dvz8opOJww$$BLBZxZJkz$wzpZ*rvZk8X 'X#+ttZqrxcbXXh'n@i'bah*c%M@QXv?, k0bJ%#vzqdZWLUUn/phB$$rLcaaQLkm@tuQW
-				ZbCd#qY0QOd$$BzCYvZ0YvZdzJJBLJYdb&-:ti xv0CZzbMUm%uw8n1UwdYZMko*#+.'[Cw80MQ*bbYZWLYQcq*Y@$@ZJzb*JdQ0@qXJw
-				MB&@@8M%B8B$$$B$B%&%@B$BB@W%%8@@&WMdL  )%B%B%%B&&@B%@%8BBBWB$M-&u' 1#B%@8@&B%B&8@@#BB%%8@$$%B%B8%@@B$8%%&
-				kwucWb0LXQ0$$BzoJt0JmxMZxO0*jrCd#p|/}   1LQ_/m*rx$$$Bobabbbno[ c^  h#mCaY8bzcZOZWJvXf/pmB$@fYrbhYJw0@tjYo
-				aMbM8*baho#$$Bbkbpaakpo#dkbBakb#M%baC^  La>^O#8zi%$@B@hxb#bJ!' L" _&8koBa8h&##da%adap*&b@$@okp#&bMaa@*bbo
-				W%%B8&W8BB@@B@%8B&%B%%BB&%&BBB88&8M*b!` (h^`f#%-,*MWMZJltMo1   Y^^Q&%@%%WW8B8W8%%8#%B%@%8@%&%%%&$%8%%B&W&
-				LYhqvJXp@@bqUmcrUtOWUXaqXvbOOMvzzz"  [c  {/f .b@@q.` np}  "J  'wpwJrU%woY?ZhXzMQxvfLh@@M/wY[C0X|hJjzcWrtx
-				BB@BBBB@$@@BBBBBBBB@BB@@@B@BB@BBBBj  vm  |Bn  u$@Z   W8I  iY  JB@@BBB@B$@%BBBB@BBBBB@@$@BBB%@@BB@BBBB@BBB
-				*&bddZkd&B@BB8W&*#MW8B#pZ#8*akkh8Mvl'>xi".J/<.lCM0[ .vJ(' ~1} vZWhmqwo&wmbkdoqdWZwaab88@%a&bo#b&b8%Md*8p#
-				x0-f)tOLU@@%*ZmLCxJbm8opYJapLq0fhYoC-.`Y~ "I)^ '0@b   [#. .nk  tMdY}|rWz/t||Qt)r1ckCvdM@B|L|Xm1f}O8zfb8z/
-				@@$@@@@@@@$@@@@$@@@@@@@@@@@@@$@@@@@@81Ipa. i8l .(@h   Cq  ^qa }@@@@@@@@@@@@@$@@@$@@@@@$$@@$@@$@@@@$@@@@@@
-				vdoYL*#bU@$@%*&#ahhh&B#wJdo#BaaXk#bbLqvYY< `c: .I@h   wt  lk8+vm%aUzvOWqL|zhmCComo8ZhmM$@OMLh*Y*0W%0oM%0J
-				mUnv/ChL@@$v0vdWZuJd&L1c*apftJ#dQZnxJQY0_  |; .>&o` ~/x ^ua0JOdavuYo*Zr|u#ubxX[ujL&Zp@$@8Lh*dOpaw&&wOz#aw
-				@@&%@BB%@$$B@BB%B@@@@B@%B%BBB88BB%BM@B@@#i 1X  Ioo' tBkcmB@@@@8BBB@BB%@&@@M&BB%B@B@B&$$$BBB@%8@@B@BB@%B%B
-				bmLnj]kh@$$(mnQ#Lzmk#(rQ8L|(1a8Yt?f0w)vdLt .n  ;aW> ncJUpZ%pqpfwJJo#&cXUQMtvUhCf{jOcz$$@oY&%O*whL@w)+b8Lm
-				#bZqLpWq$$$qwLbWphqbBhQ0WWaOmp&*baQmmZa#Oprrm} <8%QjJQwZZpphdaO&wqZ#*om0qMQp0kLqQp%wq@$@%0bWdO#&w8%#0p&8m
-				@@B@@@@@@$$@@$@@@@@@$@@@@@@@@@@@@@@a$@$@@@@@$%z{$$@@@@$@@@$@@@@@@@@@@@@@@@B@@@@@@@@@@@$$@@@@@@B@@$$@@@@@@
-				cdXmbZvYM$$|O/0*cJYp#/cZ&U)vLma/)cZ1mfuL||XC(bW@$%d*bXOYQmo*o0u0kwawwtkwOLjLkk(whntzXo$@azk8qwwbcBU/fm&c/
-				WWMW8Mo#@$$#MoWBMooWB*aMB&#o#MB#oo*h&ooao*8&#8B$$BWWWoMa*W88&%WWWWW%%*&#%%**8%aMWo#Ma@$$BM8B&W88MB%WMM@8*
-				**/
-
 				defaultCamZoom = 0.68;
                 hogBg = new BGSprite('hog/bg', 0, 0, 1.1, 0.9);
                 hogBg.scale.x = 1.5;
@@ -7983,7 +7870,7 @@ class PlayState extends MusicBeatState
 						{
 							case 5, 9, 12, 634, 639, 642, 646, 650, 654, 710, 716, 774, 780, 838, 845, 895, 900, 905, 910, 1472, 1476, 1480, 1484:
 								festSpinFull();
-							case 64, 69/**SEX**/, 73, 77, 383, 389, 393, 397, 448, 452, 456, 460, 512, 516, 520, 524, 576, 580, 584, 588, 664, 698, 729, 760, 790, 857:
+							case 64, 69, 73, 77, 383, 389, 393, 397, 448, 452, 456, 460, 512, 516, 520, 524, 576, 580, 584, 588, 664, 698, 729, 760, 790, 857:
 								festSpinOppenet();
 							case 408, 410, 412, 472, 474, 476, 536, 538, 540, 600, 602, 604, 682, 710, 745, 808, 825, 872, 888:
 								festSpinPlayer();
@@ -8903,88 +8790,3 @@ class PlayState extends MusicBeatState
 	}
 
 }
-
-
-				/**%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&8BB#%%%%WW%BaLccccccvccccccccccccccwbW%8
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%8%%@Wb%B%88M@BbcccccccXb&ccccccccccccczdd%o%
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&%@mdb@o8WBBpUcccccccwad8XccccccccccccObk@M%
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%WW@*CqbM@M&%kUcccccccJbqcQMLcccccccccccXdba@8%
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%W%Bpcwbb@@BMYcvcccccc0MXccJkqcccccccccccUbb*B8%
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%WBBLcXdbhBWLccccccccckZzccczbbcccccccccccOddMB8%
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%&%#YczpbhWwzccccccccUoQcvccczp#cccccccccczddb8&%%
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%&%WcccCbo#JvccccccccC*zcccccccmMUcccccccccJbbdBM%%
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%8BBCcccp%OcvcccccccuQocvvccccccUoqcccccccccObbk@*%%
-				   %%%%%%%%%%%%%%W8%&%%%%%%%%M@Occc0aUccccccccccQaXccccccccczdocccccccccqbbhBM%%
-				   %%%%%%%%%%%%%WB&B8W8B%%%%&@accccOzccccccccccQ#zcvcccccccccm#ccccccccYbbb#8%%%
-				   %%%%%%%%%%%%%oBmb8@@o8%%%W8cccccccvcccccccczovccccccccccccQoUcccccccQdbb#B%%%
-				   %%%%%%%%%%%%8&WzCphM@B%oW@dvccccccccccccccccccccccccccccccYhbcccccccwbbb&%%%%
-				   %%%%%%%%%%%%%@acczQpk#&B@Wqcccccccccccccccccccccccccccccccvw%zccccccpbbb8&MMM
-				   %%%%%%%%%%%%%BdcccccYZqpkahmcccccccccccccccccccccccccccccccJMCccccczdddd&%%%%
-				   %%%%%%%%%%%%%BmcccccccczzzccccccccccccccccccccccccccccccccczbmccccczbdpLXXXXX
-				   %%%%%%%%%%%%8%mcccccccccccccccccccccccccccccccccccccccccXCzcZ*ccccccQJccccccc
-				   %%%%%%%%%%%%8%Qccccccccccccccccccccccccccccccccccccccccc*@pcv#Ccccccccccccccc
-				   %%%%%%%%%%%%8%QcccccccccccccccccccccccccccccccccccccccccO@WCcObcccccccccccccc
-				   %%%%%%%8o%BBB@QcccccccccccccccccccccccccccccccccccccccccczYaQcwcccccccccccccc
-				   %%%8&8&@B8#oaWQccccccccccccccccccccccccccccccccccccccccccccYMJccccccccccccccc
-				   %%W&@@&d00000kQcccccccccccccccccccccccccccccczzUL00JXcvcccccz8Xcccccccccccccc
-				   #%B8bm0000000h0ccccccccccccccccccccccccccccYdokmZOOmh#pXccccvk*cccccccccccccc
-				   @&k0000000000wmccccccccccccccccccccccccccYqkZCCJCCCCCJd#aqzccU@dccccccccccccc
-				   *000000000000mmvcccccccccccccccccccccvccQWCCCCCCCCCCCLCLM&#CZ*BBUcccccccccccc
-				   #000000000000ZdcccccccccccccccccccccczcpdLCCCCCCCCCCCJJCCWBhB@@BWcccccccccccc
-				   WbZwpwZZOO0000dcccccccccccccccccccccczqdCCCCCCCCCCJCCCCCJL&@@@Bo&LcccccccccYw
-				   W%#ooooaaobwmOoccccccccccccccccccccccL#JCJCCCCXcnnrruxcYCJm@@@@*ohccccccccCdd
-				   W&B*ooooooooaa#cccccccccccczcccccccvwWCJJCJU)+<<<<<>>><<~(X*@@@#*&UccccccOdbb
-				   &&&%Moooooooa&0ccccccccccccvvccczCZM%hCCY|[><><>>>>>>>>>>>>|@@@W*Maccccccpbbb
-				   &&&&%Wooooooo#cccccccX0mmmO0MOk&*#M@@CUr~<<]xuxj-<<>>>>>>>>+8@@B8&WccccccJbbb
-				   &WW&&8%ooo*8BJccccvcJWqCCCb8@@%@@@@@*X!>~+mZII:1*C+<<>><><>>O@@@**W0ccccccZbb
-				   B&&&&&&%%%@@&ccccccwhLJCCCCCd@@@@@@@C+>i]h}::::;;-m1<>>>><<>)@@@&*oWzvccccYbb
-				   *8%&&&W&BB&@qcccccQoCCCJCJCJJL#@@@@@u>><CI:[1[l::;iL(>>>>>>>+B@@@*o#qccccccpd
-				   oo*%%&&BB*M%ccccXY*CCCCCUc(<<<]B@@@@?>>~Y,<Mho8f;::!b~<<>>>><%@@@W##8Cccccc0b
-				   oooo*%@@#%B#cvCZwMdCJCU{+>><><<[B@@@-<>u|;aO0L0wb:::_U<<>>>>>M@@@%MW8#cccccJb
-				   %8&#o*BBW%BMcYB8@BQCCf+>>><<<~>>f@B@_>>01:M00QQQwbI;;p[<>>>><#@@@B***WZcccccd
-				   &W&BBBq8B&#@mhB@@%CY[<><<<<QWWm1>X@@-><qI:#0Qw0QQkt::;v>>>>>>d@@@@#o*oMcccccq
-				   &W&&&Wmw@@&#@@B@@&U+>>><>>)*LL0Wt+@@-><C):#OQ%p000*:;IC<>>>>>d@@@@#o*oWUccccm
-				   &W&W%mZZqBW&@@@$@8?>>>><><t0a/MO*jt@?>>c/;*O0O@ZQQk|:;Z+>>>>>d@@$@#*o**qccccm
-				   M888&ZZZmd@&WB@@@B_>>>>>>>tO#;>&Ob<#n>><Y;XqQQ&hQQLU;:C_>>>>>*@@@@Mo***Bccccm
-				   mZmpoBqmZk@&%a@@@@j>>>><<>]M#_:>pq/kn>><w:Id0Qq#QQQJ:;C_>>>><%@$@&&M**o%Ycccm
-				   ZZZZZZmZ#@M8%%&@@@L<><><>>+Bmx::mO&]n<<<X<;d0QQZQQLJ:;O<<>>>?@@@@#*WW**8Ucccm
-				   ZZZZZZqMBW&8%%8&$@#~>><<>>>wmQ;:?od]<><>]wIvpQQQ0QOu:+c>>>><d@@@Wo**8&*WJcccm
-				   mZZwwk@BoB%%%%%&%@@r>>>>>>>_hd+;I#0)>>><<U?;k00Q0Qh_;h[>><>?@@@%***oo8&&Jcccw
-				   &88MaB%8%%%%%%%%&%@*~<>>>>>>qwM!I#O)<><>><b]i&OQLorIO/<><><U@@8*ao*o**&%Jvccq
-				   LLLCLk8&%%%%%%%%%W8@u<>>>>>>-hOokq0{<<>>>><Of>vLJt_0u>><>>)B8a*hqppa**o@Xcczd
-				   LLLLCZ@M%%%%%%%%%%&@B]<>>>>>>(#ZQO#~f#Mm}><<Xb({)Jd[<><>>{d*0Q0&0QQQOboBXccJb
-				   LLLLCLB8%%%%%%%%%%%W@mi<<>>><<+J8m+thQ0ObM}><<---<>><<<>wMOQQQQZo0Q000Z8cccOb
-				   LLLLCC&8%%%%%%%%%%%%*@J>>><>>>><~>to00000Zd#Lf<<<><<>t0Mb00QQQQQ*qQL0Qmmcczpb
-				   LLLLCLWW%%%%%%%%%%%&B8&v+><<>><>_nM000000000qMB&ZOO&&8%#h00QQ0QQL#O0QQpYccUdb
-				   LLLLLLo&%%%%%%%%%%%#@paW%c-<~_-b%a00000000Q0O0qWB8*kbhkQMmQQ0L00Lp*QQLacvcmbd
-				   CCLLLCo8BB%%%%%%%%W@M0Oq*8BWW&odm0000000000000QOOZO0m#0QOWQQQQQQQQhdLOkccUbbb
-				   CCCCmpM%%&8%%%%%%%&BZ0000mbdwOO00000000000000000000wM0Q0QbbQQQQQQ00#mbCzcwbbb
-				   0LUX@@@&&@%*M&%%%8%Ww000000000000000000000000000Q0wM0QQ0LQ#OQQQQQQQ0MhccQdbbb
-				   h?+~k@@#+>nb@@@@@@@@*00000000000000000000O00O00Zh%hOQQQQQQOMQQQQQQ0QbmcUddbbb
-				   ]<~<(@@$BapW$@$$$@@8O000000000000000000Oko##*ahhq0o0QQQ0QQQkpQQQQQQQ#XYddbdbd
-				   O/(f*@@$$$$$$$$$@$%@p00000000000000OOq#MpWQQQQQQQQapQQQQQ0QO%OQQ0Q0O0XpbbbkhW
-				   h@@@8W&@$$$$$$$$Bo&*@p0000000000000O8bCQQmbQQQQ0Q00#Q0Q0Q0QQQo0QLQQ8zqbbdM%ab
-				   @*%%8%8&&W&&&&&W8%%&%BbOO00000000OwohQ0Q0Q#m0QQQQQQpa0QQ0QQQQhdQ0QbQpba8#abbb
-				   &%%%%%%%%%%%%%%%%%%%W8BkO00000000Z&ZWZQQQQL#00QQQQ0QomQQQQQ0QQ*OQmMboW8Wbdbbb
-				   8%%%%%%%%%%%%%%%%%%%%%*@8mOQ00000w00mW0QQQ0kaQQQ0QQQO80Q0QQ0QQwWL&oBMoWbbbbbb
-				   %%%%%%%%%%%%%%%%%%%%%%%WBB*Z000000000qW0QQL0a0QQQ0Q0QpbQQQ0Q0Q0p8%#oo*8bbbbbb
-				   %%%%%%%%%%%%%%%%%%%%%%%%W8%@oq00000000d*000QO&OQQ0QQ0Q*wQ0QQQQ0oMWooo&adbbbbb
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%&oBB%kZ00000OdaO0QQwkQLQQQ0QO#QQQQQQ&kbWoo##dbdbbbb
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%8&M&@%&*mO00Om&O0QL#q00QQQ0Qdk0QQw8&ado%8Mbdddbbbb
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#@##%M*ohkp8#O0Qo0QQQQQQQ*ZOh&oo8kbkhbbbbbbbbW
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%8%B*adbk&#&&WWhmq*0QQQQQ0mW8*oao*Wbdddbbbbbk%@
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%o@@adbh#ooooo*W%WdLCCpo%W*aooooo%qzXpbbbbbbk%
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&@kbbB*aooooaoo##MMM#*ooooaoooo8LccXqbbbbbbk
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%BBbbdBoooooooooooooooooaoooooaw#LcccQbbbbbbb
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%BdbbBoooooooooooooooooooooadO0MYcccYdbdbbbb
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Bbbb@oooooooooooooooooooadZ00OMcccvXbdbbbbb
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%BBbbb%*ooooooooooooooaahpZ0O00qZcccccddbbbbb
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%8@bdbkMooooooooooaoahbZ000000OMXcccczdbbbbbb
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%M@adbb8oaoooooooaqmO00000000OhOcccccYbbbbbbd
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%8Bobbd#Mooooooooh00000000000qkccccccCbbbbba&
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&Bbddd&*oooooooZ0000000000mazccccccZ#kbdaB@
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%k@o#bba%*ooooooO000000000mWXccccccccZM8adbk
-				   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%W&@%*hbbda&ooooooO00000000wazccccccccYddkoW*b**/
-
-
-					// SECRET NORMAL INVASION!!!!!!
