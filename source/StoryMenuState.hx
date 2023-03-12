8f8ee478c0317ff7798f43edf9802b04155de49e
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-12 14:21:13 +0200

    update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -199,6 +199,10 @@ class StoryMenuState extends MusicBeatState
 		sprDifficulty.offset.x = 70;
 		sprDifficulty.y = leftArrow.y + 10;
 
+    #if android
+    addVirtualPad(FULL, A_B)
+    #end
+
 		super.create();
 	}
 
