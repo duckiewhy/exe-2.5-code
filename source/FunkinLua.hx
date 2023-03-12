f9dc508d81bf194410565fa42d45b4af9f125549
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-12 11:25:56 +0200

    update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -1118,7 +1118,9 @@ class FunkinLua {
 			luaTrace('musicFadeOut is deprecated! Use soundFadeOut instead.', false, true);
 		});
 
+   #if desktop
 		Discord.DiscordClient.addLuaCallbacks(lua);
+	 #end
 
 		call('onCreate', []);
 		#end
