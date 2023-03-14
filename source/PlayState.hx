91370f0e33608bd1ec3d9e5e60669fcf36313ad3
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-14 13:33:04 +0200

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6758,7 +6758,7 @@ class PlayState extends MusicBeatState
 			video.destroy();
 		}
 		video.playVideo(Paths.video(name));
-		video.readyCallback = function(){
+		video.openingCallback = function(){
 			video.visible=true;
 		}
 		add(video);
