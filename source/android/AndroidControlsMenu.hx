c2dedeec4bb1c0cab4d164aa86fc9b6e4251bf86
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-15 15:29:02 +0200

    Add files via upload

--- source/android/AndroidControlsMenu.hx
+++ source/android/AndroidControlsMenu.hx
@@ -43,6 +43,12 @@ class AndroidControlsMenu extends MusicBeatState
 		bg.antialiasing = ClientPrefs.globalAntialiasing;
 		add(bg);
 
+		var titleText:Alphabet = new Alphabet(75, 60, "Android Controls", true);
+		titleText.scaleX = 0.6;
+		titleText.scaleY = 0.6;
+		titleText.alpha = 0.4;
+		add(titleText);
+
 		vpad = new FlxVirtualPad(RIGHT_FULL, NONE, 0.75, ClientPrefs.globalAntialiasing);
 		vpad.alpha = 0;
 		add(vpad);
@@ -126,7 +132,7 @@ class AndroidControlsMenu extends MusicBeatState
 			save();
 			FlxTransitionableState.skipNextTransIn = true;
 			FlxTransitionableState.skipNextTransOut = true;
-			MusicBeatState.switchState(new OptionsState());
+			MusicBeatState.switchState(new options.OptionsState());
 		}
 		#end
 	}
