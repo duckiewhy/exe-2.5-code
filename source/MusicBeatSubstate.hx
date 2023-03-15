f231b51dceb0b26d5ef2c5d5284291c8e42fc30a
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-15 15:46:07 +0200

    update MusicBeatSubstate.hx

--- source/MusicBeatSubstate.hx
+++ source/MusicBeatSubstate.hx
@@ -1,14 +1,14 @@
 package;
 
+#if android
+import flixel.input.actions.FlxActionInput;
+import android.FlxVirtualPad;
+#end
 import Conductor.BPMChangeEvent;
 import flixel.FlxG;
 import flixel.FlxSubState;
 import flixel.FlxBasic;
 import flixel.FlxSprite;
-#if android
-import flixel.input.actions.FlxActionInput;
-import android.FlxVirtualPad;
-#end
 
 class MusicBeatSubstate extends FlxSubState
 {
