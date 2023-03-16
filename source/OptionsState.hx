d3c984b4ced8506c42502710b9641eaae9b26ace
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-16 18:08:51 +0200

    update OptionsState.hx

--- source/OptionsState.hx
+++ source/OptionsState.hx
@@ -32,7 +32,7 @@ using StringTools;
 // TO DO: Redo the menu creation system for not being as dumb
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Controls', 'Preferences'];
+	var options:Array<String> = ['Controls', 'Mobile Controls', 'Preferences'];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;
