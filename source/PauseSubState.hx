d40a84641a14fa5ed14a05b416a83f226754d400
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-17 18:34:14 +0200

    update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -223,7 +223,7 @@ class PauseSubState extends MusicBeatSubstate
 		FlxTween.tween(iconP1, {angle: 0}, 0.8, {ease: FlxEase.circOut});
 		FlxTween.tween(timeBar, {alpha: 1}, 0.5, {ease: FlxEase.circOut});
 		cameras = [camThing];
-
+                #if mobile addVirtualPad(UP_DOWN, A_B); #end
 	}
 
 	override function update(elapsed:Float)
