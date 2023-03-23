af348bff1fd9c40149acffe0abbe4a4035048df8
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-23 15:39:09 +0200

    new things

--- source/mobile/flixel/FlxHitbox.hx
+++ source/mobile/flixel/FlxHitbox.hx
@@ -32,34 +32,30 @@ class FlxHitbox extends FlxSpriteGroup
 		
 		//altpos = ClientPrefs.dodgepos; not yet
   
-    if(isdodge && PlayState.SONG.song.toLowerCase() == 'chaos'){
-     if(!altpos){
-     add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0xFF00FF));
-     add(buttonDown = createHint(FlxG.width / 4, 0, Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0x00FFFF));
-     add(buttonDodge = createHint(0, Std.int(FlxG.height / 4) * 3, FlxG.width, Std.int(FlxG.height / 4), 0xFF7700));
-     add(buttonUp = createHint(FlxG.width / 2, 0, Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0x00FF00));
-     add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), 0, Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0xFF0000));
-     }else{
-     add(buttonLeft = createHint(0, Std.int(FlxG.height / 4), Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0xFF00FF));
-     add(buttonDown = createHint(FlxG.width / 4, Std.int(FlxG.height / 4), Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0x00FFFF));
-     add(buttonDodge = createHint(0, 0, FlxG.width, Std.int(FlxG.height / 4), 0xFF7700));
-     add(buttonUp = createHint(FlxG.width / 2, Std.int(FlxG.height / 4), Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0x00FF00));
-     add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), Std.int(FlxG.height / 4), Std.int(FlxG.width / 4), Std.int(FlxG.height / 4) * 3, 0xFF0000));
-     }
-    }
-    else if(ring && PlayState.SONG.song.toLowerCase() == 'triple-trouble'){
+   switch (type){
+    case DODGE:
+    {
     add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 5), FlxG.height, 0xFF00FF));
 		add(buttonDown = createHint(FlxG.width / 5, 0, Std.int(FlxG.width / 5), FlxG.height, 0x00FFFF));
 		add(buttonDodge = createHint(FlxG.width / 2.5, 0, Std.int(FlxG.width / 5), FlxG.height, 0xFFD000));
 		add(buttonUp = createHint(FlxG.width / 2.5 + (FlxG.width / 5), 0, Std.int(FlxG.width / 4), FlxG.height, 0x00FF00));
-		add(buttonRight = createHint(FlxG.width / 2.5 * 2, 0, Std.int(FlxG.width / 4), FlxG.height, 0xFF0000)); 
-    }else{
+		add(buttonRight = createHint(FlxG.width / 2.5 * 2, 0, Std.int(FlxG.width / 4), FlxG.height, 0xFF0000));
+   }
+    case DEFAULT:
+    {
     add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 4), FlxG.height, 0xFF00FF));
 		add(buttonDown = createHint(FlxG.width / 4, 0, Std.int(FlxG.width / 4), FlxG.height, 0x00FFFF));
 		add(buttonUp = createHint(FlxG.width / 2, 0, Std.int(FlxG.width / 4), FlxG.height, 0x00FF00));
 		add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), 0, Std.int(FlxG.width / 4), FlxG.height, 0xFF0000));
-    }
-    
+   }
+		default:
+		{
+    add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 4), FlxG.height, 0xFF00FF));
+		add(buttonDown = createHint(FlxG.width / 4, 0, Std.int(FlxG.width / 4), FlxG.height, 0x00FFFF));
+		add(buttonUp = createHint(FlxG.width / 2, 0, Std.int(FlxG.width / 4), FlxG.height, 0x00FF00));
+		add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), 0, Std.int(FlxG.width / 4), FlxG.height, 0xFF0000));
+		}
+  }
 
 		scrollFactor.set();
 	}
@@ -114,4 +110,9 @@ class FlxHitbox extends FlxSpriteGroup
 		#end
 		return hint;
 	}
+}
+
+  enum HitboxType {
+    DEFAULT;
+    DODGE;
 }
