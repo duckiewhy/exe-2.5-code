afd48058c6f598947c8206c1b0cb3df659fce46f
MasterX <95196453+Default-name123@users.noreply.github.com>
2023-03-24 20:10:23 -0500

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -116,11 +116,13 @@ class CoolUtil
 
 	//uhhhh does this even work at all? i'm starting to doubt
 	public static function precacheSound(sound:String, ?library:String = null):Void {
-		if(!Assets.cache.hasSound(Paths.sound(sound, library))) {
-			FlxG.sound.cache(Paths.sound(sound, library));
-		}
+		Paths.sound(sound, library);								 
+   
 	}
 
+	 public static function precacheMusic(sound:String, ?library:String = null):Void {
+		Paths.music(sound, library);
+	}
 	public static function browserLoad(site:String) {
 		#if linux
 		Sys.command('/usr/bin/xdg-open', [site]);
